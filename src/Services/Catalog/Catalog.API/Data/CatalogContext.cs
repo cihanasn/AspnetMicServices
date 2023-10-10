aa8f54e4a8a50ca86375c992f739fc11d5fed810
cihanasn <cihanasan1@gmail.com>
2023-10-10 20:10:26 +0300

    Create a database for Catalog API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        }
+        public IMongoCollection<Product> Products => throw new NotImplementedException();
+    }
+}
